066cd1d8d4f9b5bdbf5b01fa36f9cb06585659a3
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-12-04 16:31:58 -0300

    Add files via upload

--- Exercicio2.c
+++ Exercicio2.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    int elevadorA_matutino = 0, elevadorA_vespertino = 0, elevadorA_noturno = 0;
-    int elevadorB_matutino = 0, elevadorB_vespertino = 0, elevadorB_noturno = 0;
-    int elevadorC_matutino = 0, elevadorC_vespertino = 0, elevadorC_noturno = 0;
-
+main() {
+    int elevadorA_matutino = 0, elevadorA_vespertino = 0, elevadorA_noturno = 0, totalA;
+    int elevadorB_matutino = 0, elevadorB_vespertino = 0, elevadorB_noturno = 0, totalB;
+    int elevadorC_matutino = 0, elevadorC_vespertino = 0, elevadorC_noturno = 0 ,totalC;
     int totalMatutino = 0, totalVespertino = 0, totalNoturno = 0, totalServicos = 0;
+    float mediaUtilizacao;
 
-    int moradores = 50;
+    char elevador, periodo;
 
-    printf("Informe o uso dos elevadores (A, B ou C) e o período (M, V ou N) para cada morador:\n");
+    for (int i = 0; i < 5; ++i) {
 
-    for (int i = 0; i < moradores; ++i) {
-        char elevador, periodo;
-        printf("Morador %d: ", i + 1);
-        scanf(" %c %c", &elevador, &periodo);
+        printf("\nDigite o elevador que mais usa (A, B ou C): ");
+        scanf("%c", &elevador);
+        fflush(stdin);
+
+        printf("Digite o periodo em que mais usa (M, V ou N): ");
+        scanf("%c", &periodo);
+        fflush(stdin);
 
         switch (elevador) {
             case 'A':
@@ -33,17 +36,105 @@ int main() {
                 else if (periodo == 'N') elevadorC_noturno++;
                 break;
             default:
-                printf("Elevador ou período inválido para o morador %d.\n", i + 1);
+                printf("\nElevador ou período inválido.");
         }
     }
+
+    totalA = elevadorA_matutino + elevadorA_noturno + elevadorA_vespertino;
+    totalB = elevadorB_matutino + elevadorB_noturno + elevadorB_vespertino;
+    totalC = elevadorC_matutino + elevadorC_noturno + elevadorC_vespertino;
+    totalServicos = totalA + totalB + totalC;
 
     totalMatutino = elevadorA_matutino + elevadorB_matutino + elevadorC_matutino;
     totalVespertino = elevadorA_vespertino + elevadorB_vespertino + elevadorC_vespertino;
     totalNoturno = elevadorA_noturno + elevadorB_noturno + elevadorC_noturno;
     totalServicos = totalMatutino + totalVespertino + totalNoturno;
 
-    // Restante do código para calcular e exibir resultados...
-    // (mantive a lógica dos cálculos e exibições similar aos exemplos anteriores)
+    if(totalMatutino > totalVespertino && totalMatutino > totalNoturno){
+        printf("\nPeriodo mais utilizado: matutino");
 
-    return 0;
+        if(elevadorA_matutino > elevadorB_matutino && elevadorA_matutino > elevadorC_matutino){
+        printf("\nElevador mais utilizado nesse periodo: A");
+        } else if(elevadorB_matutino > elevadorA_matutino && elevadorB_matutino > elevadorC_matutino){
+        printf("\nElevador mais utilizado nesse periodo: B");
+        } else {
+        printf("\nElevador mais utilizado nesse periodo: C");
+        }
+
+    } else if(totalVespertino > totalMatutino && totalVespertino > totalNoturno){
+        printf("\nPeriodo mais utilizado: vespertino");
+
+        if(elevadorA_vespertino > elevadorB_vespertino && elevadorA_vespertino > elevadorC_vespertino){
+        printf("\nElevador mais utilizado nesse periodo: A");
+        } else if(elevadorB_vespertino > elevadorA_vespertino && elevadorB_vespertino > elevadorC_vespertino){
+        printf("\nElevador mais utilizado nesse periodo: B");
+        } else {
+        printf("\nElevador mais utilizado nesse periodo: C");
+        }
+    } else {
+        printf("\nPeriodo mais utilizado: noturno");
+
+        if(elevadorA_noturno > elevadorB_noturno && elevadorA_noturno > elevadorC_noturno){
+        printf("\nElevador mais utilizado nesse periodo: A");
+        } else if(elevadorB_noturno > elevadorA_noturno && elevadorB_noturno > elevadorC_noturno){
+        printf("\nElevador mais utilizado nesse periodo: B");
+        } else {
+        printf("\nElevador mais utilizado nesse periodo: C");
+        }
+
+    }
+
+    if(totalA > totalB && totalA > totalC){
+        if(totalB > totalC){
+                mediaUtilizacao = (float)totalB / totalServicos;
+            } else {
+                mediaUtilizacao = (float)totalC / totalServicos;
+            }
+        printf("\nElevador mais utilizado: A");
+
+        if(elevadorA_matutino > elevadorA_vespertino && elevadorA_matutino > elevadorA_noturno){
+        printf("\nPeriodo mais utilizado: matutino");
+        } else if(elevadorA_vespertino > elevadorA_matutino && elevadorA_vespertino > elevadorA_noturno){
+        printf("\nPeriodo mais utilizado: vespertino");
+        } else {
+        printf("\nPeriodo mais utilizado: nnoturno");
+        }
+
+        } else if(totalB > totalA && totalB > totalC){
+            if(totalA > totalC){
+                mediaUtilizacao = (float)totalA / totalServicos;
+            } else {
+                mediaUtilizacao = (float)totalC / totalServicos;
+            }
+        printf("\nElevador mais utilizado: B");
+
+        if(elevadorB_matutino > elevadorB_vespertino && elevadorB_matutino > elevadorB_noturno){
+        printf("\nPeriodo mais utilizado: matutino");
+        } else if(elevadorB_vespertino > elevadorB_matutino && elevadorB_vespertino > elevadorB_noturno){
+        printf("\nPeriodo mais utilizado: vespertino");
+        } else {
+        printf("\nPeriodo mais utilizado: nnoturno");
+        }
+
+        } else {
+            if(totalA > totalB){
+                mediaUtilizacao = (float)totalA / totalServicos;
+            } else {
+                mediaUtilizacao = (float)totalB / totalServicos;
+            }
+        printf("\nElevador mais utilizado: C");
+
+        if(elevadorC_matutino > elevadorC_vespertino && elevadorC_matutino > elevadorC_noturno){
+        printf("\nPeriodo mais utilizado: matutino");
+        } else if(elevadorC_vespertino > elevadorC_matutino && elevadorC_vespertino > elevadorC_noturno){
+        printf("\nPeriodo mais utilizado: vespertino");
+        } else {
+        printf("\nPeriodo mais utilizado: nnoturno");
+        }
+        }
+
+        mediaUtilizacao = mediaUtilizacao * 100;
+
+        printf("\nPercentagem sobre o total de servicos prestados do elevador de media utilizacao: %f", mediaUtilizacao);
+
 }
